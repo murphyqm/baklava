7ef9b11092294eafc26b780d5e766bd1157e618b
Maeve <earmmu@leeds.ac.uk>
2024-09-03 14:01:30 +0100

    Initial release

---
+++ src/baklava/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about baklava."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Maeve Murphy Quinlan"
+__email__ = "earmmu@leeds.ac.uk"
+__version__ = "0.1.0"
